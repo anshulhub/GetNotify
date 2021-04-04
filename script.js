18c1dcfba416ffca22002084a1d9f90bad630d13
Anshul <anshulgarg712000@gmail.com>
2021-04-04 19:06:29 +0530

    data retrieved

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 const button = document.querySelector(".button-container");
 
 
+const newnote = (text = '') => {
+    const note = document.createElement('div');
+    note.classList.add("note");
+
+    const html = ` <div class="modify">
+<button class="edit"><i class="fas fa-edit"></i></button>
+<button class="delete"><i class="fas fa-trash-alt"></i></button>
+</div>
+<div class="save ${text ? "" : "hidden"}"></div>
+<textarea class="${text ? "hidden" : ""}"></textarea> `
+
+    note.insertAdjacentHTML("afterbegin", html)
+
+    const edit = note.querySelector(".edit");
+    const remove = note.querySelector(".delete");
+    const save = note.querySelector(".save");
+    const textarea = note.querySelector("textarea");
+
+    remove.addEventListener('click', () => {
+        note.remove();
+    })
+
+    edit.addEventListener("click", () => {
+        save.classList.toggle("hidden");
+        textarea.classList.toggle("hidden");
+    })
+
+    textarea.addEventListener("change", (event) => {
+        const text = event.target.value;
+        save.innerHTML = text;
+    })
 
 
 
 
-button.addEventListener("click",newnote())
+
+    document.body.appendChild(note);
+}
+
+
+
+button.addEventListener("click", () => newnote())
